5b3ac0b8b270f27fbc12a6f8bd08e8b1e6a694d3
Schuler <Schuler@C-SCHULER>
2023-04-24 12:33:02 +0200

    Added new Action

---
+++ OCROperator/Models/Interface/Action/FileToZammad.cs
@@ -0,0 +1,20 @@
+﻿using OCROperator.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCROperator.Models.Interface.Action
+{
+    internal class FileToZammad : IAction
+    {
+        public string Settings { get; set; }
+        public ILogger Logger { get; set; }
+        public MailFactory MailFactory { get; set; }
+        public bool Execute(PapercutItem Item, string Text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
